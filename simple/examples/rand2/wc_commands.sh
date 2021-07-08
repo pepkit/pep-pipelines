0ba2935cdf559e272b055a4a53fb45ae51e12628
nsheff <nsheff@users.noreply.github.com>
2021-07-08 16:14:59 -0400

    remove outputs

--- simple/examples/rand2/wc_commands.sh
+++
@@ -1,3 +0,0 @@
-# Pipeline started at 09-11 15:49:30
-
-wc -l /scratch/jps3dp/tools/databio//pep-pypiper/simple/examples/rand2.txt
--- simple/examples/rand3/wc_commands.sh
+++
@@ -1,3 +0,0 @@
-# Pipeline started at 09-11 15:49:31
-
-wc -l /scratch/jps3dp/tools/databio//pep-pypiper/simple/examples/rand3.txt
